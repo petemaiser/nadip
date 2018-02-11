cd4534a6e58b839a386a572e4b0433c95ba7bde3
Pete Maiser <pete@maiser.com>
2018-02-11 17:02:36 -0600

    Initial Commit

---
+++ nadip/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nadip
+//
+//  Created by Pete Maiser on 2/11/18.
+//  Copyright © 2018 Pete Maiser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
